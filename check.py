402e78e54fef648bf91f3d453745e525f5cbf97e
SHI <shihu223@gmail.com>
2024-10-15 10:57:23 +0800

    更新签到得积分

--- check.py
+++ check.py
@@ -1,7 +1,11 @@
 import requests
+import random
+import time
 
 
 class Check:
+    SERVERID = ['82967fec9605fac9a28c437e2a3ef1a4', 'b9fc7bd86d2eed91b23d7347e0ee995e',
+                'e3fa93b0fb9e2e6d4f53273540d4e924', 'd3936289adfff6c3874a2579058ac651']
     headers = {'Host': 'wechat.v2.traceint.com', 'Connection': 'keep-alive', 'Origin': 'http://tuia.sensezeal.com',
                'User-Agent': 'Mozilla/5.0 (Linux; Android 11; M2012K11AC Build/RKQ1.200826.002; wv) AppleWebKit/537.36 '
                              '(KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/3149 MMWEBSDK/20211001 Mobile '
@@ -11,16 +15,19 @@ class Check:
                'Referer': 'http://tuia.sensezeal.com/', 'Accept-Encoding': 'gzip, deflate, br',
                'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7'}
     list_body = {"operationName": "getList",
-                 "query": "query getList {userAuth{credit{tasks{id task_id task_name task_info task_url credit_num "
-                          "contents conditions task_type status}}}}"}
-    check_body = {"operationName": "done2",
-                  "query": "mutation done2($user_task_id: Int!) {userAuth {credit {done2(user_task_id: "
-                           "$user_task_id)}}}",
-                  "variables": {"user_task_id": 96981628}}
+                 "query": "query getList {\n userAuth {\n credit {\n tasks {\n id\n task_id\n task_name\n task_info\n "
+                          "task_url\n credit_num\n contents\n conditions\n task_type\n status\n }\n staticTasks "
+                          "{\n id\n name\n task_type_name\n credit_num\n contents\n button\n }\n }\n }\n}"}
+    check_body = {"operationName": "done",
+                  "query":"mutation done($user_task_id: Int!) {\n userAuth {\n credit {\n done(user_task_id: "
+                          "$user_task_id)\n }\n }\n}",
+                  "variables": {"user_task_id": 98318215}}
 
     def __init__(self, cookie):
-        self.cookie = cookie[14:]
-        self.headers['Authorization'] = self.cookie
+        self.cookie = cookie + '; v=5.5; Hm_lvt_7ecd21a13263a714793f376c18038a87=1713417820,1714277047,1714304621,1714376091; ' \
+                               'Hm_lpvt_7ecd21a13263a714793f376c18038a87=' + str(int(time.time() - 1)) + '; SERVERID=' + \
+                      random.choice(self.SERVERID) + '|' + str(int(time.time() - 1)) + '|1714376087'
+        self.headers['Cookie'] = self.cookie
 
     def check_in(self):
         r = requests.post("https://wechat.v2.traceint.com/index.php/graphql/", json=self.list_body,
